1543be4c1d80e45e72217ec1a4f6b01844e69d4d
nicka <nickallee8529@gmail.com>
2023-08-11 15:48:36 -0500

    added main file print.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Solution mySolution = new Solution();
-        
-        int[] inputer = new int[3];
-        inputer[0] = 3;
-        inputer[1] = 2;
-        inputer[2] = 4;
-        System.out.println(Arrays.toString(mySolution.twoSum(inputer, 6)));
-        int[] inputer2 = new int[2];
-        inputer2[0] = 3; inputer2[1] = 3;
-        System.out.println(Arrays.toString(mySolution.twoSum(inputer2, 6)));
+        System.out.println("Nicholas Allee - nicholascallee@gmail.com");
     }
 }
