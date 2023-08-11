25e89b4e5ff06f851798969a6b849ed6c9d9aa90
nicka <nickallee8529@gmail.com>
2023-08-11 15:46:08 -0500

    added twoSum test. Failing right now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,8 @@ public class Main {
         inputer[1] = 2;
         inputer[2] = 4;
         System.out.println(Arrays.toString(mySolution.twoSum(inputer, 6)));
+        int[] inputer2 = new int[2];
+        inputer2[0] = 3; inputer2[1] = 3;
+        System.out.println(Arrays.toString(mySolution.twoSum(inputer2, 6)));
     }
 }
--- src/test/java/LeetCodeTests.java
+++ src/test/java/LeetCodeTests.java
@@ -30,8 +30,15 @@ public class LeetCodeTests{
     @Test
     public void LeetCodeTestTwoSum() {
         int[] inputer = new int[3];
+        int[] correct = new int[2];
+        correct[0]= 2; correct[1] = 1;
         inputer[0] = 3; inputer[1] = 2; inputer[2] = 4;
-        System.out.println(Arrays.toString(mySolution.twoSum(inputer, 6)));
+        assertEquals(mySolution.twoSum(inputer, 6), correct);
+        correct[0]= 0; correct[1] = 1;
+        int[] inputer2 = new int[2];
+        inputer2[0] = 3; inputer2[1] = 3;
+        assertEquals(mySolution.twoSum(inputer2, 6),correct);
+
     }
 
 }
