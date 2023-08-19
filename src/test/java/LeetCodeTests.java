b5fa133ba33d19a4f93a27d75716ee3148838949
nicka <current-nicholascallee@gmail.com>
2023-08-19 12:19:57 -0500

    pow test still failing

--- src/test/java/LeetCodeTests.java
+++ src/test/java/LeetCodeTests.java
@@ -97,8 +97,11 @@ public class LeetCodeTests{
         //https://leetcode.com/problems/powx-n/
 
         assertEquals(mySolution.myPow(2, -2), .25);
-        //assertEquals(mySolution.myPow(34.00515, -3), 0.00003);
         assertEquals(mySolution.myPow(2, 10), 1024);
+        //TODO: Fix this test
+        assertEquals(mySolution.myPow(34.00515, -3), 0.00003);
+        assertEquals(mySolution.myPow(0.44528, 0), 1);
+
     }
 
     @Test
