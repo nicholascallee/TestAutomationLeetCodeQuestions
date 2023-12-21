2f816b48927e71262068e1597869da73ce092464
nicholascallee <nicholascallee@gmail.com>
2023-12-20 21:23:53 -0600

    two pointers work

--- src/main/java/org/example/Neetcode/Blind75/TwoPointers.java
+++ src/main/java/org/example/Neetcode/Blind75/TwoPointers.java
@@ -1,5 +1,9 @@
 package org.example.Neetcode.Blind75;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TwoPointers {
 
     public boolean isPalindrome(String s) {
@@ -28,5 +32,41 @@ public class TwoPointers {
             left ++;
         }
         return true;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> returner = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int x =0; x < nums.length; x++){
+            if(x > 0 && nums[x] == nums[x-1]){
+                continue;
+            }
+
+            int left = 0;
+            int right = nums.length -1;
+
+            while(left < right){
+                if(0 == nums[x] + nums[left] + nums[right]) {
+                    returner.add(Arrays.asList(nums[x], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+                else if (0 <= nums[x] + nums[left] + nums[right]){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return returner;
     }
 }
--- src/test/java/Neetcode/Blind75/TwoPointersTests.java
+++ src/test/java/Neetcode/Blind75/TwoPointersTests.java
@@ -3,8 +3,10 @@ package Neetcode.Blind75;
 import org.example.Neetcode.Blind75.TwoPointers;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 public class TwoPointersTests {
     TwoPointers mySolution = new TwoPointers();
@@ -22,5 +24,30 @@ public class TwoPointersTests {
         assertTrue(mySolution.isPalindromeTwoPointersApproach("A man, a plan, a canal: Panama"));
         assertFalse(mySolution.isPalindromeTwoPointersApproach("race a car"));
         assertTrue(mySolution.isPalindromeTwoPointersApproach(" "));
+    }
+    @Test
+    public void TwoPointersTestThreeSum() {
+        List<List<Integer>> correctValue = new ArrayList<>();
+        List<Integer> triplet1 = new ArrayList<>();
+        List<Integer> triplet2 = new ArrayList<>();
+        triplet1.add(-1);
+        triplet1.add(-1);
+        triplet1.add(2);
+        correctValue.add(triplet1);
+        triplet2.add(-1);
+        triplet2.add(0);
+        triplet2.add(1);
+        correctValue.add(triplet2);
+
+
+        List<List<Integer>> correctValue2 = new ArrayList<>();
+        List<Integer> triplet3 = new ArrayList<>();
+        triplet3.add(0);
+        triplet3.add(0);
+        triplet3.add(0);
+        correctValue2.add(triplet3);
+
+        assertEquals(correctValue,mySolution.threeSum(new int[]{-1,0,1,2,-1,-4}));
+        assertEquals(correctValue2,mySolution.threeSum(new int[]{0,0,0}));
     }
 }
